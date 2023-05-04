aed92c604229407188b8c28fb216b8574b1597d4
armyant0920 <armyant0920@gmail.com>
2023-05-04 17:02:46 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace BatchTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb_board = new System.Windows.Forms.RichTextBox();
+            this.ctrl_panel = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ipconfigToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.telnetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.puingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nslookupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+
+            this.xcopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.toolTip_ipconfig = new System.Windows.Forms.ToolTip(this.components);
+            this.btn_suspend = new System.Windows.Forms.Button();
+
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.toolTip_ipconfig = new System.Windows.Forms.ToolTip(this.components);
+            this.xcopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_board
+            // 
+            this.tb_board.Location = new System.Drawing.Point(32, 179);
+            this.tb_board.Name = "tb_board";
+            this.tb_board.ReadOnly = true;
+            this.tb_board.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.tb_board.Size = new System.Drawing.Size(726, 259);
+            this.tb_board.TabIndex = 0;
+            this.tb_board.Text = "";
+            // 
+            // ctrl_panel
+            // 
+            this.ctrl_panel.AutoScroll = true;
+            this.ctrl_panel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrl_panel.Location = new System.Drawing.Point(32, 51);
+            this.ctrl_panel.Name = "ctrl_panel";
+            this.ctrl_panel.Size = new System.Drawing.Size(699, 122);
+            this.ctrl_panel.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ipconfigToolStripMenuItem,
+            this.telnetToolStripMenuItem,
+            this.puingToolStripMenuItem,
+            this.nslookupToolStripMenuItem,
+            this.xcopyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.ShowItemToolTips = true;
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ipconfigToolStripMenuItem
+            // 
+            this.ipconfigToolStripMenuItem.Name = "ipconfigToolStripMenuItem";
+            this.ipconfigToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.ipconfigToolStripMenuItem.Text = "ipconfig";
+            this.ipconfigToolStripMenuItem.ToolTipText = "查詢本機網路資訊";
+            this.ipconfigToolStripMenuItem.Click += new System.EventHandler(this.ipconfigToolStripMenuItem_Click);
+            // 
+            // telnetToolStripMenuItem
+            // 
+            this.telnetToolStripMenuItem.Name = "telnetToolStripMenuItem";
+            this.telnetToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.telnetToolStripMenuItem.Text = "telnet";
+            this.telnetToolStripMenuItem.ToolTipText = "遠端操作";
+            this.telnetToolStripMenuItem.Click += new System.EventHandler(this.telnetToolStripMenuItem_Click);
+            // 
+            // puingToolStripMenuItem
+            // 
+            this.puingToolStripMenuItem.Name = "puingToolStripMenuItem";
+            this.puingToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.puingToolStripMenuItem.Text = "ping";
+            this.puingToolStripMenuItem.ToolTipText = "查連線狀況";
+            this.puingToolStripMenuItem.Click += new System.EventHandler(this.puingToolStripMenuItem_Click);
+            // 
+            // nslookupToolStripMenuItem
+            // 
+            this.nslookupToolStripMenuItem.Name = "nslookupToolStripMenuItem";
+            this.nslookupToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.nslookupToolStripMenuItem.Text = "nslookup";
+            this.nslookupToolStripMenuItem.ToolTipText = "查網域或IP";
+            // 
+
+            // xcopyToolStripMenuItem
+            // 
+            this.xcopyToolStripMenuItem.Name = "xcopyToolStripMenuItem";
+            this.xcopyToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.xcopyToolStripMenuItem.Text = "xcopy";
+            this.xcopyToolStripMenuItem.ToolTipText = "使用xcopy複製檔案";
+            this.xcopyToolStripMenuItem.Click += new System.EventHandler(this.xcopyToolStripMenuItem_Click);
+            // 
+
+
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(32, 444);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+
+            // btn_suspend
+            // 
+            this.btn_suspend.Location = new System.Drawing.Point(144, 443);
+            this.btn_suspend.Name = "btn_suspend";
+            this.btn_suspend.Size = new System.Drawing.Size(75, 23);
+            this.btn_suspend.TabIndex = 5;
+            this.btn_suspend.Text = "suspend";
+            this.btn_suspend.UseVisualStyleBackColor = true;
+            this.btn_suspend.Click += new System.EventHandler(this.btn_suspend_Click);
+
+            // xcopyToolStripMenuItem
+            // 
+            this.xcopyToolStripMenuItem.Name = "xcopyToolStripMenuItem";
+            this.xcopyToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.xcopyToolStripMenuItem.Text = "xcopy";
+            this.xcopyToolStripMenuItem.ToolTipText = "使用xcopy複製檔案";
+            this.xcopyToolStripMenuItem.Click += new System.EventHandler(this.xcopyToolStripMenuItem_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+
+            this.Controls.Add(this.btn_suspend);
+
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.ctrl_panel);
+            this.Controls.Add(this.tb_board);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "bat指令";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox tb_board;
+        private System.Windows.Forms.Panel ctrl_panel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ipconfigToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem telnetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem puingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nslookupToolStripMenuItem;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.ToolTip toolTip_ipconfig;
+        private System.Windows.Forms.ToolStripMenuItem xcopyToolStripMenuItem;
+
+        private System.Windows.Forms.Button btn_suspend;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BatchTool
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ipconfig_Click(object sender, EventArgs e)
+        {
+            string info = Get_IPCONFIG();
+            WriteMessage(info);
+
+        }
+
+        private void WriteMessage(string msg)
+        {
+            tb_board.AppendText(msg);
+            tb_board.SelectionStart = tb_board.Text.Length;
+            tb_board.ScrollToCaret();
+        }
+
+
+
+        public static string Get_IPCONFIG()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "ipconfig.exe";
+            process.StartInfo.Arguments = "/all";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.UseShellExecute = false;
+
+            process.Start();
+            string info = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            process.Close();
+
+            return info;
+
+
+        }
+
+        private void ipconfigToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string s = Get_IPCONFIG();
+            WriteMessage(s);
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            tb_board.Clear();
+            WriteMessage("clear!");
+        }
+
+
+
+        private void RunCMD(string args) {
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = string.Format(@"/c {0}",args);//c是一個cmd.exe命令行選項，它告訴cmd.exe在執行完指定的命令後退出。
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            WriteMessage(output);
+            process.WaitForExit();
+            //process.Close();            
+            //return output;
+
+        }
+
+        private void ResetPanel(List<Control> controls) {
+            //清空所有控制項
+            ctrl_panel.Controls.Clear();
+            foreach(Control control in controls){
+                ctrl_panel.Controls.Add(control);
+            }           
+        }
+
+        private void Telnet() {
+
+            List<Control> controls = new List<Control>();
+            TextBox tb1 = new TextBox();
+            TextBox tb2 = new TextBox();
+            Button btn = new Button();           
+            ToolTip tip1 = new ToolTip();
+            tip1.SetToolTip(tb1, "輸入欲查詢IP或網域");
+            
+            ToolTip tip2 = new ToolTip();
+            tip2.SetToolTip(tb2,"輸入port號");
+           
+           
+            //button setting
+            btn.Text = "telnet";
+            btn.Click += (sender, e) =>
+            {
+                RunCMD(string.Format("{0} {1} {2}", btn.Text, tb1.Text.Trim(),tb2.Text.Trim()));
+            };
+            
+            tb1.Location = new Point(0, 0);
+            tb2.Location = new Point(0, 25);
+            btn.Location = new Point(0, 50);
+
+            controls.Add(btn);
+            controls.Add(tb2);
+            controls.Add(tb1);
+            //設定共同事件
+            foreach (Control ctrl in controls)
+            {
+                ctrl.TabStop = true;//允許tab
+                ctrl.KeyDown += (sender, e) =>
+                {
+                    if (e.KeyCode == Keys.Enter)
+                    {
+                        btn.PerformClick();
+                    }
+
+                };
+
+            }
+         
+            ResetPanel(controls);
+
+        }
+
+
+        private void Xcopy()
+        {
+
+            List<Control> controls = new List<Control>();
+            TextBox tb1 = new TextBox();
+            TextBox tb2 = new TextBox();
+            Button btn = new Button();
+            ToolTip tip1 = new ToolTip();
+            tip1.SetToolTip(tb1, "輸入複製路徑");
+
+            ToolTip tip2 = new ToolTip();
+            tip2.SetToolTip(tb2, "輸入目的路徑");
+
+            CheckBox cb1 = new CheckBox();
+            cb1.Text = "複製所有子目錄，但不包括空目錄";
+            cb1.Tag = "/s";
+            cb1.Location = new Point(100, 0);
+            //
+            CheckBox cb2 = new CheckBox();
+            cb2.Text="複製所有子目錄，包括空目錄";
+            cb2.Location = new Point(100, 25);
+            cb2.Tag = "/e";
+            //
+            CheckBox cb3 = new CheckBox();
+            cb3.Text = "複製隱藏和系統檔案";
+            cb3.Location = new Point(100, 50);
+            cb3.Tag = "/h";
+            //
+            CheckBox cb4 = new CheckBox();
+            cb4.Text = "複製屬性。如果您不指定此選項，則新檔案將不會繼承舊檔案的屬性";
+            cb4.Location = new Point(100, 75);
+            cb4.Tag = "/k";
+            //
+            CheckBox cb5 = new CheckBox();
+            cb5.Text = "不提示您確認是否要覆寫已存在的目的地檔案";
+            cb5.Location = new Point(100, 100);
+            cb5.Tag = "/y";
+            //
+            CheckBox cb6 = new CheckBox();
+            cb6.Text = "僅在源檔案比目的地檔案新或源檔案比目的地檔案舊時才複製";
+            cb6.Location = new Point(100, 125);
+            cb6.Tag = "/d";
+            cb6.BackColor = Color.Green;
+            cb6.AutoSize = true;
+            cb6.Width = 600;
+
+            //
+
+            controls.Add(cb1);
+            controls.Add(cb2);
+            controls.Add(cb3);
+            controls.Add(cb4);
+            controls.Add(cb5);
+            controls.Add(cb6);
+
+            //設定checkbox格式
+            foreach (CheckBox cb in controls.Where(c => (c is CheckBox))) {
+                cb.AutoSize = true;
+                cb.BackColor = Color.DarkGray;
+            
+            }
+
+
+            //button setting
+            btn.Text = "XCOPY";
+            btn.Click += (sender, e) =>
+            {
+                // /e：複製所有子目錄，包括空目錄。 
+                // /h：複製隱藏和系統檔案。
+                // /y：不提示您確認是否要覆寫已存在的目的地檔案。
+                // /d：僅在源檔案比目的地檔案新或源檔案比目的地檔案舊時才複製。
+
+                string settings = string.Empty;
+                //加入所有有勾選的checkbox
+                foreach (CheckBox cb in controls.Where(c => (c is CheckBox) ))
+                {
+                    if (cb.Checked)
+                        settings += string.Format("{0} ",cb.Tag.ToString());
+                
+                }
+                RunCMD(string.Format("xcopy {0} {1} {2}", tb1.Text.Trim(), tb2.Text.Trim(),settings));
+            };
+            
+            tb1.Location = new Point(0, 0);
+            tb2.Location = new Point(0, 25);
+            btn.Location = new Point(0, 50);
+
+            controls.Add(btn);
+            controls.Add(tb2);
+            controls.Add(tb1);
+            //設定共同事件
+            foreach (Control ctrl in controls.Where(c=>!(c is CheckBox)))//不是checkbox的加入enter事件
+            {
+                //ctrl.Dock = DockStyle.Top;
+                ctrl.KeyDown += (sender, e) =>
+                {
+                    if (e.KeyCode == Keys.Enter)
+                    {
+                        btn.PerformClick();
+                    }
+
+                };
+
+            }
+
+
+
+            ResetPanel(controls);
+
+        }
+
+
+
+        private void Ping() {
+            List<Control> controls = new List<Control>();
+            TextBox tb1 = new TextBox();
+            Button btn = new Button();
+            tb1.Dock = DockStyle.Left;
+            tb1.KeyDown += (sender, e) => {
+                if (e.KeyCode == Keys.Enter) {
+                    btn.PerformClick();
+                }
+            
+            };
+
+            controls.Add(tb1);
+            
+            btn.Text = "Send";
+            btn.Dock = DockStyle.Right;
+            
+            btn.Click += (sender, e) =>
+            {
+                RunCMD("ping " + tb1.Text.Trim());
+            };
+            controls.Add(btn);
+            ResetPanel(controls);
+        }
+
+        private void AddControls() {
+
+            TextBox tb1 = new TextBox();
+            tb1.Dock = DockStyle.Left;
+
+            ctrl_panel.Controls.Add(tb1);
+            Button btn = new Button();
+            btn.Text = "Send";
+            btn.Dock = DockStyle.Right;
+            ctrl_panel.Controls.Add(btn);
+            btn.Click += (sender, e) => {
+                RunCMD("telnet "+tb1.Text.Trim());                        
+            };
+            ctrl_panel.Controls.Add(btn);
+            
+            
+        
+        }
+
+        private void telnetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Telnet();
+        }
+
+        private void puingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Ping();
+        }
+
+        private void xcopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Xcopy();
+        }
+
+
+        private void btn_suspend_Click(object sender, EventArgs e)
+        {
+            Application.SetSuspendState(PowerState.Suspend, false, false);//設定休眠狀態為Suspend,避免強制關機及顯示鎖定
+        }
+
+    }
+    
+}
